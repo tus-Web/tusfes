a2a867b6b78eb3b8c670c8e333eda5870b9f35b5
健斗 尾宮 <jiandouweigong@gmail.com>
2025-10-24 18:06:24 +0900

    Change pagination

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,27 +7,32 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import './styles.css';
 
-import { Pagination } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default function Home() {
     return(
     <>
         <Swiper
+            loop={true}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
             slidesPerView={1.2}
-            spaceBetween={10}
+            spaceBetween={50}
             centeredSlides={true}
             pagination={{
                 clickable: true,
             }}
-            modules={[Pagination]}
+            modules={[Autoplay, Pagination]}
             className="mySwiper"
         >
-            <SwiperSlide><img src="public\img\tmp_img1.jpg" alt="tmp_img1" /></SwiperSlide>
-            <SwiperSlide>img 2 here</SwiperSlide>
-            <SwiperSlide>img 3 here</SwiperSlide>
-            <SwiperSlide>img 4 here</SwiperSlide>
-            <SwiperSlide>img 5 here</SwiperSlide>
-            <SwiperSlide>img 6 here</SwiperSlide>
+            <SwiperSlide><img src="/img\tmp_img1.jpg" alt="tmp_img1" /></SwiperSlide>
+            <SwiperSlide><img src="/img\tmp_img2.jpg" alt="tmp_img2" /></SwiperSlide>
+            <SwiperSlide><img src="/img\tmp_img3.jpg" alt="tmp_img3" /></SwiperSlide>
+            <SwiperSlide><img src="/img\tmp_img4.jpg" alt="tmp_img4" /></SwiperSlide>
+            <SwiperSlide><img src="/img\tmp_img5.jpg" alt="tmp_img5" /></SwiperSlide>
+            <SwiperSlide><img src="/img\tmp_img6.jpg" alt="tmp_img6" /></SwiperSlide>
         </Swiper>
     </>
     );

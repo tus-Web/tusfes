c57421a711407f3634e57e55841c2c09fdc63eb3
matapaku <matapaku0119@gmail.com>
2025-12-07 14:46:39 +0900

    脆弱性対策のバージョンアップデート

--- next.config.js
+++ next.config.js
@@ -55,9 +55,6 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: { unoptimized: true },
 };
 
